9f60031250d2459fc2265e23638d67466853178b
Daniella <mariano.dani@hotmail.com>
2026-03-12 19:24:29 -0300

    Creating Enemy Creating EnemyShot Creating Player Creating PlayerShot

--- code/Const.py
+++ code/Const.py
@@ -16,9 +16,12 @@ ENTITY_SPEED = {
     'Level1Bg6': 6,
     'Player1': 3,
     'Player2': 3,
-    'Enemy1': 2,
-    'Enemy2': 1
-
+    'Player1Shot': 3,
+    'Player2Shot': 3,
+    'Enemy1': 1,
+    'Enemy2': 1,
+    'Enemy1Shot': 5,
+    'Enemy2Shot': 2
 }
 
 EVENT_ENEMY = pygame.USEREVENT +1
@@ -36,10 +39,16 @@ ENTITY_HEALTH ={
     'Player2': 300,
     'Player2Shot': 1,
     'Enemy1': 50,
-    'Enemy1Shot': 1,
     'Enemy2': 60,
+    'Enemy1Shot': 1,
     'Enemy2Shot': 1
+}
 
+ENTITY_SHOT_DELAY = {
+    'Player1': 20,
+    'Player2': 15,
+    'Enemy1': 100,
+    'Enemy2': 200
 }
 
 #M
--- code/EntityMediator.py
+++ code/EntityMediator.py
@@ -1,5 +1,8 @@
+from code.Const import WIN_WIDTH
 from code.Enemy import Enemy
 from code.Entity import Entity
+from code.Player import Player
+from code.PlayerShot import PlayerShot
 
 
 class EntityMediator:
@@ -7,7 +10,10 @@ class EntityMediator:
     @staticmethod
     def __verify_collision_window(ent: Entity):
         if isinstance(ent, Enemy):
-            if ent.rect.right < 0:
+            if ent.rect.right <= 0:
+                ent.health = 0
+        if isinstance(ent, PlayerShot):
+            if ent.rect.left >= WIN_WIDTH:
                 ent.health = 0
 
     @staticmethod
